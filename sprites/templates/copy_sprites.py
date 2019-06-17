76296e08255f2918b22fd09e03d18a23e4aaec8a
Simon Howard <fraggle@soulsphere.org>
2019-06-16 23:09:08 -0400

    Add sprites.

---
+++ sprites/templates/copy_sprites.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import deh9000
+import shutil
+
+WEAPONS = (
+	deh9000.MT_SHOTGUN,
+	deh9000.MT_SUPERSHOTGUN,
+	deh9000.MT_CHAINGUN,
+	deh9000.MT_MISC25, # bfg
+	deh9000.MT_MISC26, # csaw
+	deh9000.MT_MISC27, # RL
+	deh9000.MT_MISC28, # plasma
+)
+
+frames = ["empty"] * len(deh9000.states)
+
+def sprite_name(state):
+	name = deh9000.sprnames[state.sprite]
+	return name + "ABCDEFGHIJKLMNOPQRSTUVWXYZ[^]"[state.frame & 0xff] + "0"
+
+def handle_weapon(mobj):
+	for state_id in deh9000.states.walk(mobj.spawnstate):
+		frames[state_id] = "weapon"
+
+def handle_pickup(mobj):
+	for state_id in deh9000.states.walk(mobj.spawnstate):
+		frames[state_id] = "item"
+
+def handle_monster(mobj):
+	for field in ("meleestate", "missilestate"):
+		for state_id in deh9000.states.walk(getattr(mobj, field)):
+			frames[state_id] = "monster_fire"
+	for field in ("deathstate", "xdeathstate", "raisestate"):
+		for state_id in deh9000.states.walk(getattr(mobj, field)):
+			frames[state_id] = "corpse"
+	for field in ("spawnstate", "seestate", "painstate"):
+		for state_id in deh9000.states.walk(getattr(mobj, field)):
+			frames[state_id] = "monster"
+
+def handle_projectile(mobj):
+	for state_id in deh9000.states.walk(mobj.spawnstate):
+		frames[state_id] = "projectile"
+
+def handle_obstacle(mobj):
+	for state_id in deh9000.states.walk(mobj.spawnstate):
+		frames[state_id] = "obstacle"
+
+for mobj_id, mobj in enumerate(deh9000.mobjinfo):
+	if mobj_id in WEAPONS:
+		handle_weapon(mobj)
+	elif mobj.flags & deh9000.MF_SPECIAL:
+		handle_pickup(mobj)
+	elif mobj.flags & deh9000.MF_COUNTKILL:
+		handle_monster(mobj)
+	elif mobj.flags & deh9000.MF_MISSILE:
+		handle_projectile(mobj)
+	elif mobj.flags & deh9000.MF_SOLID:
+		handle_obstacle(mobj)
+
+for weapon_id, weapon in enumerate(deh9000.weaponinfo):
+	for state_id in deh9000.states.walk(weapon.flashstate):
+		frames[state_id] = "gunflash"
+	for field in ("upstate", "downstate", "readystate", "atkstate"):
+		for state_id in deh9000.states.walk(getattr(weapon, field)):
+			frames[state_id] = "gun"
+
+wadinfo_names = set()
+
+for state_id, state in enumerate(deh9000.states):
+	template = frames[state_id]
+	name = sprite_name(state).lower()
+	#print("state %d: %s -> %s" % (state_id, template, name))
+	if template != "empty":
+		shutil.copy(template + ".png", "../%s.png" % name)
+	wadinfo_names.add(name)
+
+for name in sorted(wadinfo_names):
+	print(name)
+
