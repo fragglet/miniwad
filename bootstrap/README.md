021f72847b9bc9b811ee193194056029175a031b
Simon Howard <fraggle@soulsphere.org>
2019-06-29 00:33:33 -0400

    Add Freedoom bootstrap IWAD.

---
+++ bootstrap/README.md
@@ -0,0 +1,1 @@
+Minimal bootstrap "IWAD" file, from Freedoom.
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -f miniwad.wad
 deutex -iwad \
+       -doom2 bootstrap/ \
        -flats -patches -textures \
        -graphics -levels -lumps \
        -musics -sounds -textures \
